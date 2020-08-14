15cc3ce3505442c329349cefb44cad1a5d343998
Micah <mcmanyika@gmail.com>
2020-08-14 16:59:24 -0500

    change host

--- newsite/settings.py
+++ newsite/settings.py
@@ -28,10 +28,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z5u_2#s7(hx_7zxv&wdgt*&htwpq63c!7os)1c61j=)x$u15(i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 if DEBUG:
-    ALLOWED_HOSTS = ['164.90.177.56', 'www.freejacob.cf',
-                     'freejacob.cf']
+    ALLOWED_HOSTS = ['www.freejacob.cf',
+                     'freejacob.cf', '164.90.177.56']
 else:
     ALLOWED_HOSTS = ['164.90.177.56', 'www.freejacob.cf',
                      'freejacob.cf']
