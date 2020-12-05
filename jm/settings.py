cd9d66a8d7112d7f130022c40e3305ded7262d11
Micah <mcmanyika@gmail.com>
2020-12-05 10:30:06 -0600

    settings

--- jm/settings.py
+++ jm/settings.py
@@ -87,12 +87,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
-STATIC_ROOT = os.path.join(DATA_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'jm', 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'jm', 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'jm', 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'jm', 'static'),
+# )
 SITE_ID = 1
 
 TEMPLATES = [
