96a12eb57f71b547cefbe71456f70fabcfb8ef71
Partson Manyika <mcmanyika@gmail.com>
2021-05-12 15:33:23 -0500

    debug

--- jm/settings.py
+++ jm/settings.py
@@ -81,7 +81,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
+MEDIA_ROOT = os.path.join(DATA_DIR, 'static', 'img')
 STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 
 STATICFILES_DIRS = (
