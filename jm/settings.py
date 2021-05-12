87811bf5378bd9f3a2e7e1c7370bdde24aa2bb80
Partson Manyika <mcmanyika@gmail.com>
2021-05-12 16:22:59 -0500

    debug

--- jm/settings.py
+++ jm/settings.py
@@ -80,8 +80,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/img/'
-MEDIA_ROOT = os.path.join(DATA_DIR, 'static', 'img')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
 STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 
 STATICFILES_DIRS = (
