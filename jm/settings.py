aa69aa04e2be41542ef2a70e36b5f66f57fc938a
Partson Manyika <mcmanyika@gmail.com>
2021-05-13 06:30:51 -0500

    debug

--- jm/settings.py
+++ jm/settings.py
@@ -16,12 +16,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '=!hb5zko_39=1(&24az&9e@x^_t-0$%o=gcv(y40yhvbx-4w#m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 if DEBUG:
-    ALLOWED_HOSTS = ['159.65.163.6','127.0.0.1',
+    ALLOWED_HOSTS = ['127.0.0.1',
                      'www.31julymovement.com', '31julymovement.com']
 else:
-    ALLOWED_HOSTS = ['159.65.163.6','www.31julymovement.com', '31julymovement.com']
+    ALLOWED_HOSTS = ['www.31julymovement.com', '31julymovement.com']
 
 
 # Application definition
