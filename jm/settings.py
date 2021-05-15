4aaebddffcc48e1428fc363d3e7ecd069c6376af
Partson Manyika <mcmanyika@gmail.com>
2021-05-14 22:32:51 -0500

    debug

--- jm/settings.py
+++ jm/settings.py
@@ -247,14 +247,10 @@ CMS_PERMISSION = True
 CMS_PLACEHOLDER_CONF = {}
 
 if DEBUG:
-    DATABASES = {
+     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'bigboy',
-            'USER': 'db_admin',
-            'PASSWORD': 'focus@1',
-            'HOST': 'localhost',
-            'PORT': '',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 else:
