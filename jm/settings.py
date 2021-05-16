968b33f6dda787f695750c2ce2cabf621fc67255
Partson Manyika <mcmanyika@gmail.com>
2021-05-16 12:15:05 -0500

    connect

--- jm/settings.py
+++ jm/settings.py
@@ -248,18 +248,18 @@ CMS_PLACEHOLDER_CONF = {}
 
 if DEBUG:
      DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
         # 'default': {
-        #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #     'NAME': 'bigboy',
-        #     'USER': 'db_admin',
-        #     'PASSWORD': 'focus@1',
-        #     'HOST': 'localhost',
-        #     'PORT': '',
+        #     'ENGINE': 'django.db.backends.sqlite3',
+        #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         # }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'bigboy',
+            'USER': 'db_admin',
+            'PASSWORD': 'focus@1',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
     }
 else:
     DATABASES = {
